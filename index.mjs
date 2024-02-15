2c44d4debe7f0387f3e162152967cb2e70042e78
cpablop1 <cpablop1@gmail.com>
2024-02-15 12:20:49 -0600

    Added form

---
+++ index.mjs
@@ -0,0 +1,11 @@
+const btnSave = document.querySelector('#btnSave');
+const formUser = document.querySelector('#formUser');
+
+/* btnSave.addEventListener('click', ()=>{
+    console.log('You have clicked!');
+}); */
+formUser.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log('Yor have sended form');
+    console.assert.toString
+});
